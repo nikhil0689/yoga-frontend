90708161daeb3f9df990bda1a68ebb3dea454ae3
nikhil <nikhil0689@gmail.com>
2024-04-28 15:49:35 -0700

    updated user logo

--- src/features/authentication/UserAvatar.jsx
+++ src/features/authentication/UserAvatar.jsx
@@ -26,7 +26,7 @@ function UserAvatar() {
   const { sessionUser } = auth;
   return (
     <StyledUserAvatar>
-      <Avatar src={"default-user.jpg"} alt={`Avatar of`} />
+      <Avatar src={"/default-user.jpg"} alt={`Avatar of`} />
       <span>{sessionUser.firstName + " " + sessionUser.lastName}</span>
     </StyledUserAvatar>
   );
